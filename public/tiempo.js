98f1667f541011722e84c9025f979d6c45000c87
Aday Almeida Falcón <adayalmeidafalcon@MacBook-Pro-de-Aday-2.local>
2024-02-17 18:27:43 +0000

    sound and design changes

--- public/tiempo.js
+++ public/tiempo.js
@@ -1,6 +1,16 @@
-var seconds = 300; //número de segundos a contar
-function secondPassed() {
+var seconds = 30; //número de segundos a contar
+//const audio = new Audio("sound/10s.mp3");
 
+const boton = document.getElementById("empezar");
+const div = document.getElementById("inicial");
+
+boton.addEventListener("click", function(){
+  div.classList.add("ocultar");
+  var countdownTimer = setInterval(secondPassed, 1000);
+});
+
+function secondPassed() {
+  document.getElementById("inicial").style.display = "none";
   var minutes = Math.round((seconds - 30)/60); 
   var remainingSeconds = seconds % 60;
     if (remainingSeconds < 10) { 
@@ -9,11 +19,15 @@ function secondPassed() {
   document.getElementById('tiempo').innerHTML = minutes + ":" +     remainingSeconds; 
   if (seconds == 0) { 
     clearInterval(countdownTimer); 
-    alert('Se acabó el tiempo'); 
-    document.getElementById('tiempo').innerHTML = "Buzz Buzz"; 
+      refresh = setTimeout(refrescar, 5000);
+      function refrescar(){
+        clearTimeout(refresh)
+      }
   } else { 
     seconds--; 
+    if (seconds==10){
+      audio.play();
+    }
   } 
 } 
 
-var countdownTimer = setInterval(secondPassed, 1000);
